b05edbdea72c01a652f0c6e4eec8df4eae378e55
hemarakshana <hemarakshana2454@gmail.com>
2025-03-14 18:14:17 +0530

    Day 1 Java Code

---
+++ Day1.java
@@ -0,0 +1,16 @@
+package com.javachallenges;
+
+public class Day1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World....!");
+		System.out.println("Java 100Days Challenge...!");
+		System.out.println("Name: ABC");
+		System.out.println("Age: 20");
+		System.out.println("I Started learning Java Programming Language!");
+	}
+
+}
+
+
